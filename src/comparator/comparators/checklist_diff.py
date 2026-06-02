66d3bf4434ffef7b9deeecc5dff1f47e80e98154
Andrew Yoo <andrewyoo21495@gmail.com>
2026-06-02 15:11:39 +0900

    Update checklist_diff.py

--- src/comparator/comparators/checklist_diff.py
+++ src/comparator/comparators/checklist_diff.py
@@ -24,6 +24,8 @@ def _import_all_rules():
     import src.checklist.rules.ckl_01_005  # noqa: F401
     import src.checklist.rules.ckl_01_006  # noqa: F401
     import src.checklist.rules.ckl_01_007  # noqa: F401
+    import src.checklist.rules.ckl_01_008  # noqa: F401
+    import src.checklist.rules.ckl_01_009  # noqa: F401
     import src.checklist.rules.ckl_02_001  # noqa: F401
     import src.checklist.rules.ckl_02_002  # noqa: F401
     import src.checklist.rules.ckl_02_003  # noqa: F401
